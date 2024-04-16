47d19943c1f1d7259f95e493f4d20f9cc53ca1af
Leanne13430 <Leanne.Llena@student.lut.fi>
2024-04-16 16:45:41 +0300

    Tee

--- app/src/main/java/com/example/harkkatyprojekti/TabPagerAdapter.java
+++ app/src/main/java/com/example/harkkatyprojekti/TabPagerAdapter.java
@@ -1,4 +1,35 @@
 package com.example.harkkatyprojekti;
 
-public class TabPagerAdapter {
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+public class TabPagerAdapter extends FragmentStateAdapter {
+    public TabPagerAdapter(@NonNull FragmentActivity fragmentActivity) {
+        super(fragmentActivity);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        switch (position) {
+            case 0:
+                return new FragmentPerusTiedot();
+            case 1:
+                return new FragmentVertailu();
+            case 2:
+                return new FragmentVisa();
+            default:
+                return new FragmentPerusTiedot();
+
+
+        }
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 3;
+    }
 }
