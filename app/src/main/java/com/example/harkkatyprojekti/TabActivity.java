efbe73d5343326c6ef69503b443c114f73e209d5
Leanne13430 <Leanne.Llena@student.lut.fi>
2024-04-16 15:59:21 +0300

    Tee

--- app/src/main/java/com/example/harkkatyprojekti/TabActivity.java
+++ app/src/main/java/com/example/harkkatyprojekti/TabActivity.java
@@ -1,41 +1,24 @@
 package com.example.harkkatyprojekti;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-import androidx.fragment.app.Fragment;
 
-public class MainActivity extends AppCompatActivity {
+public class TabActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_tab);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-
-
-
     }
-
-    public void switchToTabactivity(View view) {
-        Intent intent = new Intent(this, TabActivity.class);
-        startActivity(intent);
-    }
-
-
 }
